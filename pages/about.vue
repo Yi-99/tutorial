069ba5c1115fd509609caafe3d455e9456919f74
Yi-99 <ylim.8299@gmail.com>
2023-06-12 13:35:22 -0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <h2>About</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquid vero distinctio saepe eveniet corporis
+      natus illum excepturi nam repudiandae et, cum quam unde voluptas? Illo unde ducimus quia veritatis.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae ipsa aliquid accusantium illum quam error
+      commodi, modi, nihil inventore laudantium quia ut voluptatum sint, dolores laborum earum omnis maiores.
+      Inventore!</p>
+    <div>{{ data }}</div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptate velit et quas! Magni non neque maxime
+      illo, ullam tempora dolores natus voluptas, unde magnam, explicabo fugit ut totam adipisci!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
